a242c8dc0e4e9f89d38c7cc20b84e886999b3e75
carmel-schreier <89860235+carmel-schreier@users.noreply.github.com>
2022-09-11 14:41:30 +0300

    static structure

---
+++ client/src/components/Status/Status.tsx
@@ -0,0 +1,5 @@
+function Status() {
+  return <span className="badge bg-success text-capitalize">Active</span>;
+}
+
+export default Status;
---
+++ client/src/components/Table/Table.tsx
@@ -0,0 +1,35 @@
+import { Stats } from "fs";
+import Status from "../Status/Status";
+
+function Table() {
+  return (
+    <table className="table table-hover">
+      <thead>
+        <tr>
+          <th className="w-25">Full Name</th>
+          <th className="w-25">Status</th>
+          <th className="w-50">Email</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          <tr className="bg-light">
+            <td>AAA</td>
+            <td>
+              <Status />
+            </td>
+            <td>uuu@gmail.com</td>
+            <td>
+              <button className="btn btn-default">
+                <i className="bi-trash3"></i>
+              </button>
+            </td>
+          </tr>
+        }
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
