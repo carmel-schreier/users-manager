8668efcf77ab7ec82bf159bb13f1c3978952ab8e
carmel-schreier <89860235+carmel-schreier@users.noreply.github.com>
2022-09-17 17:47:36 +0300

    Added state

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,19 +1,59 @@
 import React from "react";
 
-interface HeaderProps {}
+interface HeaderProps {
+  addUser: Function;
+}
 
-interface HeaderState {}
+interface HeaderState {
+  fullName: string;
+  email: string;
+}
 
 class Header extends React.Component<HeaderProps, HeaderState> {
-  // state = { :  }
+  constructor(props: HeaderProps) {
+    super(props);
+    this.state = {
+      fullName: "",
+      email: "",
+    };
+  }
+  handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    fieldName: string
+  ) => {
+    this.setState({
+      ...this.state,
+      [fieldName]: event.target.value,
+    });
+  };
+  addUser = () => {
+    this.props.addUser({
+      fullName: this.state.fullName,
+      email: this.state.email,
+      status: "active",
+    });
+
+    this.setState(() => ({
+      fullName: "",
+      email: "",
+    }));
+  };
   render() {
     return (
       <div className="d-flex align-items-center p-3 my-4 bg-light">
         <h5 className="me-auto mb-0">Users</h5>
         <div className="d-flex">
-          <input type="text" placeholder="Full Name" className="form-control" />
+          <input
+            value={this.state.fullName}
+            onChange={(e) => this.handleInputChange(e, "fullName")}
+            type="text"
+            placeholder="Full Name"
+            className="form-control"
+          />
           <input
             type="text"
+            value={this.state.email}
+            onChange={(e) => this.handleInputChange(e, "email")}
             placeholder="Email"
             className="form-control mx-3"
           />
--- client/src/components/Users/Users.tsx
+++ client/src/components/Users/Users.tsx
@@ -12,19 +12,33 @@ export interface IUser {
 }
 interface UsersState {
   users: Array<IUser>;
-  addSuccess: boolean;
+  //addSuccess: boolean;
 }
 
-interface UsersProps {}
+//interface UsersProps {}
 
-interface UsersState {}
+interface UsersState {
+  users: Array<IUser>;
+}
 
-class Users extends React.Component<UsersProps, UsersState> {
+class Users extends React.Component<{}, UsersState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      users: [],
+    };
+  }
+
   render() {
     return (
       <div className="bg-dark bg-opacity-10 border px-2">
         <Header />
-        <Table users={[]} />
+        {this.state.users.length === 0 && (
+          <div className="alert alert-warning" role="alert">
+            No users to display
+          </div>
+        )}
+        <Table users={this.state.users} />
       </div>
     );
   }
